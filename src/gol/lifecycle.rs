b17060ce189f8c2e07f5a4c9c16843115ac83bc0
Keith Amling <keith.amling@gmail.com>
2020-03-20 14:41:01 -0700

    Start sketching gol lifecycle.

---
+++ src/gol/lifecycle.rs
@@ -0,0 +1,29 @@
+use crate::bits;
+use crate::dfs;
+
+use bits::Bits;
+use dfs::lifecycle::DfsLifecycle;
+use dfs::res::DfsResVec;
+
+pub struct GolLifecycle {
+    pub threads: usize,
+    pub recollect_ms: u64,
+}
+
+impl<B: Bits> DfsLifecycle<DfsResVec<(B, B)>> for GolLifecycle {
+    fn threads(&self) -> usize {
+        return self.threads;
+    }
+
+    fn recollect_ms(&self) -> u64 {
+        return self.recollect_ms;
+    }
+
+    fn on_recollect(&self, r: DfsResVec<(B, B)>) -> bool {
+        println!("Recollect...");
+
+        // TODO: actual status
+
+        return true;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,32 @@ mod bits;
 mod dfs;
 mod gol;
 
+use bits::Bits;
+use dfs::res::DfsResToVec;
+use gol::graph::GolGraph;
+use gol::lifecycle::GolLifecycle;
+
 fn main() {
+    main1::<u32>();
+}
+
+fn main1<B: Bits>() {
+    let ge = GolGraph {
+        mt: 3,
+        mx: 8,
+
+        ox: 0,
+        oy: 1,
+    };
+
+    let re = DfsResToVec();
+
+    let le = GolLifecycle {
+        threads: 4,
+        recollect_ms: 1000,
+    };
+
+    dfs::dfs::<(B, B), _, _, _, _>(&ge, &re, &le);
+
     println!("Hello, world!!!");
 }
