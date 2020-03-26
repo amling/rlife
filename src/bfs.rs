aa79e1e0daf3b6a0fc5bdd160aec8e346f51fdba
Keith Amling <keith.amling@gmail.com>
2020-03-26 00:39:04 -0700

    Somewhat slower but way, way more packed

--- src/bfs.rs
+++ src/bfs.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::hash::Hash;
 
 use crate::dfs;
@@ -7,56 +6,75 @@ use dfs::graph::DfsGraph;
 use dfs::lifecycle::DfsLifecycle;
 use dfs::res::DfsRes;
 
+fn materialize_path<N: Clone>(qs: &[Vec<(usize, N)>], idx: usize) -> Vec<N> {
+    let mut qs = &qs[0..(qs.len() - 1)];
+    let mut r = Vec::new();
+    let mut idx = idx;
+    while qs.len() > 0 {
+        let &(idx2, ref n2) = &qs[qs.len() - 1][idx];
+        r.push(n2.clone());
+        qs = &qs[0..(qs.len() - 1)];
+        idx = idx2;
+    }
+    r.reverse();
+    r
+}
+
+fn check_cycle<N: Eq>(qs: &[Vec<(usize, N)>], idx: usize, n: &N) -> Option<usize> {
+    let mut qs = &qs[0..(qs.len() - 1)];
+    let mut idx = idx;
+    while qs.len() > 0 {
+        let &(idx2, ref n2) = &qs[qs.len() - 1][idx];
+        if n == n2 {
+            return Some(qs.len() - 1);
+        }
+        qs = &qs[0..(qs.len() - 1)];
+        idx = idx2;
+    }
+    None
+}
+
 pub fn sbfs<N: Clone + Hash + Eq, R, GE: DfsGraph<N>, RE: DfsRes<N, R>, LE: DfsLifecycle<N, R>>(ge: &GE, re: &RE, le: &LE) {
-    let mut q: Vec<(HashMap<N, usize>, N)> = Vec::new();
+    let mut qs = vec![vec![(0, ge.start())]];
 
-    let n0 = ge.start();
+    loop {
+        let ql = qs.last().unwrap();
+        if ql.len() == 0 {
+            break;
+        }
 
-    q.push((vec![(n0.clone(), 0)].into_iter().collect(), n0));
-
-    while q.len() > 0 {
         let mut q2 = Vec::new();
         let mut r = re.empty();
-        for (already, n) in q {
-            for n2 in ge.expand(&n) {
-                let idx = already.len();
-                let mut already2 = already.clone();
-                already2.insert(n2.clone(), idx);
-
+        for (idx, &(prev_idx, ref n)) in ql.iter().enumerate() {
+            for n2 in ge.expand(n) {
                 if ge.end(&n2) {
-                    let mut path: Vec<_> = already.iter().map(|(n, &idx)| (n.clone(), idx)).collect();
-                    path.sort_by_key(|&(_, idx)| idx);
-                    let mut path: Vec<_> = path.into_iter().map(|(n, _)| n).collect();
-                    path.push(n2);
+                    let mut path = materialize_path(&qs, prev_idx);
+                    path.push(n2.clone());
                     le.debug_end(&path);
                     r = re.reduce(r, re.map_end(path));
                     continue;
                 }
 
-                if let Some(&idx) = already.get(&n2) {
-                    let mut path: Vec<_> = already.iter().map(|(n, &idx)| (n.clone(), idx)).collect();
-                    path.sort_by_key(|&(_, idx)| idx);
-                    let mut path: Vec<_> = path.into_iter().map(|(n, _)| n).collect();
-                    path.push(n2);
+                if let Some(idx) = check_cycle(&qs, prev_idx, n) {
+                    let mut path = materialize_path(&qs, prev_idx);
                     let cycle = path.drain(idx..).collect();
                     le.debug_cycle(&path, &cycle);
                     r = re.reduce(r, re.map_cycle(path, cycle));
                     continue;
                 }
 
-                q2.push((already2, n2));
+                q2.push((idx, n2));
             }
         }
-        let firstest = match q2.first() {
-            Some(&(ref already, _)) => {
-                let mut path: Vec<_> = already.iter().map(|(n, &idx)| (n.clone(), idx)).collect();
-                path.sort_by_key(|&(_, idx)| idx);
-                let mut path: Vec<_> = path.into_iter().map(|(n, _)| n).collect();
+        qs.push(q2);
+        let firstest = match qs.last().unwrap().first() {
+            Some(&(idx, ref n)) => {
+                let mut path = materialize_path(&qs, idx);
+                path.push(n.clone());
                 path
             },
             None => vec![],
         };
         le.on_recollect(firstest, r);
-        q = q2;
     }
 }
