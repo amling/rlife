d7a0b447d9b6e7f9ce6640ff544a5ec9bb93fd1e
Keith Amling <keith.amling@gmail.com>
2020-04-14 16:19:04 -0700

    Scalar set_bit and get_bit

--- ds/src/scalar.rs
+++ ds/src/scalar.rs
@@ -1,3 +1,5 @@
+#![allow(unused_parens)]
+
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::ops::BitAnd;
@@ -38,6 +40,17 @@ pub trait Scalar: ScalarMarker {
     fn one() -> Self;
     fn from_usize(c: usize) -> Self;
     fn to_usize(self) -> usize;
+
+    fn set_bit(&mut self, idx: usize, v: bool) {
+        *self &= !(Self::one() << idx);
+        if v {
+            *self |= (Self::one() << idx);
+        }
+    }
+
+    fn get_bit(&self, idx: usize) -> bool {
+        (*self & (Self::one() << idx)) != Self::zero()
+    }
 }
 
 // marker for unsigned
