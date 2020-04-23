ab1aae78ff7a9a19803aa68ec85febc6ece666e2
Keith Amling <keith.amling@gmail.com>
2020-04-23 15:56:38 -0700

    Parallel deepen

--- src/bfs/bfs2.rs
+++ src/bfs/bfs2.rs
@@ -1,3 +1,5 @@
+use crossbeam::queue::PopError;
+use crossbeam::queue::SegQueue;
 use std::collections::VecDeque;
 
 use crate::bfs;
@@ -11,7 +13,7 @@ use dfs::graph::DfsNode;
 use dfs::lifecycle::DfsLifecycle;
 use dfs::res::DfsRes;
 
-pub fn bfs2<N: DfsNode, R, GE: DfsGraph<N>, RE: DfsRes<N::KN, R>, LE: DfsLifecycle<N, R>>(n0: N, ge: &GE, re: &RE, le: &mut LE) {
+pub fn bfs2<N: DfsNode, R, GE: DfsGraph<N> + Sync, RE: DfsRes<N::KN, R>, LE: DfsLifecycle<N, R>>(n0: N, ge: &GE, re: &RE, le: &mut LE) {
     let mut kns;
     let mut q;
     let mut q_foresight;
@@ -74,7 +76,7 @@ pub fn bfs2<N: DfsNode, R, GE: DfsGraph<N>, RE: DfsRes<N::KN, R>, LE: DfsLifecyc
                 q2.push_back((prev_idx, n2, kn2));
             }
 
-            compact(ge, &mut kns, &mut q, &mut q_foresight, &mut q2, &mut q2_foresight);
+            compact(ge, le.threads(), &mut kns, &mut q, &mut q_foresight, &mut q2, &mut q2_foresight);
         }
 
         // Step two: fold q2 over into kns and q3
@@ -87,7 +89,7 @@ pub fn bfs2<N: DfsNode, R, GE: DfsGraph<N>, RE: DfsRes<N::KN, R>, LE: DfsLifecyc
             }
             q3.push_back((prev_idx, n));
 
-            compact(ge, &mut kns, &mut q3, &mut q3_foresight, &mut q2, &mut q2_foresight);
+            compact(ge, le.threads(), &mut kns, &mut q3, &mut q3_foresight, &mut q2, &mut q2_foresight);
         }
 
         eprintln!("Completed BFS step {} => {}", q_size, q3.len());
@@ -106,7 +108,7 @@ pub fn bfs2<N: DfsNode, R, GE: DfsGraph<N>, RE: DfsRes<N::KN, R>, LE: DfsLifecyc
     }
 }
 
-fn compact<N: DfsNode, GE: DfsGraph<N>>(ge: &GE, kns: &mut KnPile<N::KN>, qa: &mut VecDeque<(usize, N)>, qa_foresight: &mut usize, qb: &mut VecDeque<(usize, N, Option<N::KN>)>, qb_foresight: &mut usize) {
+fn compact<N: DfsNode, GE: DfsGraph<N> + Sync>(ge: &GE, threads: usize, kns: &mut KnPile<N::KN>, qa: &mut VecDeque<(usize, N)>, qa_foresight: &mut usize, qb: &mut VecDeque<(usize, N, Option<N::KN>)>, qb_foresight: &mut usize) {
     // whatever kns thinks plus (usize, usize) for space during recompaction
     loop {
         let kns_size = kns.len() * (kns.esize() + std::mem::size_of::<(usize, usize)>());
@@ -116,11 +118,11 @@ fn compact<N: DfsNode, GE: DfsGraph<N>>(ge: &GE, kns: &mut KnPile<N::KN>, qa: &m
             return;
         }
 
-        deepen(ge, "qa", qa, qa_foresight, |&(idx, ref n)| {
+        deepen(ge, threads, "qa", qa, qa_foresight, |&(idx, ref n)| {
             let path = kns.materialize_cloned(idx);
             (Path::from_vec(path), n.clone())
         });
-        deepen(ge, "qb", qb, qb_foresight, |&(idx, ref n, ref kn)| {
+        deepen(ge, threads, "qb", qb, qb_foresight, |&(idx, ref n, ref kn)| {
             let mut path = kns.materialize_cloned(idx);
             if let Some(kn) = kn {
                 path.push(kn.clone());
@@ -139,16 +141,52 @@ fn compact<N: DfsNode, GE: DfsGraph<N>>(ge: &GE, kns: &mut KnPile<N::KN>, qa: &m
     }
 }
 
-fn deepen<T, N: DfsNode, GE: DfsGraph<N>>(ge: &GE, name: &'static str, q: &mut VecDeque<T>, q_foresight: &mut usize, mut f: impl FnMut(&T) -> (Path<N>, N)) {
+fn deepen<T: Send, N: DfsNode, GE: DfsGraph<N> + Sync, F: Fn(&T) -> (Path<N>, N) + Send + Sync>(ge: &GE, threads: usize, name: &'static str, q: &mut VecDeque<T>, q_foresight: &mut usize, f: F) {
     let t0 = std::time::Instant::now();
     let size0 = q.len();
     let foresight0 = *q_foresight;
     let foresight1 = foresight0 + 1;
-    q.retain(|t| {
-        let (mut path, n) = f(t);
+    eprintln!("Deepening {} from size {} foresight {}...", name, size0, foresight0);
 
-        deepen_search(ge, &mut path, n, foresight1)
-    });
+    let shards = threads * 10;
+    let mut q1s: Vec<_> = (0..shards).map(|i| {
+        let ct = ((i + 1) * size0 / shards) - (i * size0 / shards);
+        let q1: VecDeque<_> = q.drain(0..ct).collect();
+        q1
+    }).collect();
+
+    {
+        let wq = SegQueue::new();
+        for q1 in q1s.iter_mut() {
+            wq.push(q1);
+        }
+
+        crossbeam::scope(|sc| {
+            for _ in 0..threads {
+                sc.spawn(|_| {
+                    loop {
+                        let q1 = match wq.pop() {
+                            Ok(q1) => q1,
+                            Err(PopError) => {
+                                return;
+                            }
+                        };
+
+                        q1.retain(|t| {
+                            let (mut path, n) = f(t);
+
+                            deepen_search(ge, &mut path, n, foresight1)
+                        });
+                    }
+                });
+            }
+        }).unwrap();
+    }
+
+    for mut q1 in q1s {
+        q.append(&mut q1);
+    }
+
     *q_foresight = foresight1;
     eprintln!("Deepened {} from size {} foresight {} to size {} foresight {} in {:?}", name, size0, foresight0, q.len(), foresight1, t0.elapsed());
 }
