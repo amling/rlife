b2511e620be383aa51ed9e629faa037a528e96da
Keith Amling <keith.amling@gmail.com>
2020-05-26 16:44:43 -0700

    StringError::label

--- ds/src/err.rs
+++ ds/src/err.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::fmt::Debug;
+use std::fmt::Display;
 
 #[derive(Debug)]
 pub struct StringError {
@@ -10,6 +11,14 @@ impl<E: Error> From<E> for StringError {
     fn from(e: E) -> StringError {
         StringError {
             msg: format!("{:?}", e),
+        }
+    }
+}
+
+impl StringError {
+    pub fn label(&self, s: impl Display) -> StringError {
+        StringError {
+            msg: format!("{}: {}", s, self.msg),
         }
     }
 }
