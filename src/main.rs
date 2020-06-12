e862b6782b6e78f0cfb1d608c82e8c1a12ad37bd
Keith Amling <keith.amling@gmail.com>
2020-06-12 12:52:33 -0700

    Back to normal main

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ use ars_ds::scalar::UScalar;
 use ars_rctl_main::rq::RctlRunQueue;
 use serde::Serialize;
 use serde::de::DeserializeOwned;
-use std::collections::HashSet;
-use std::marker::PhantomData;
 use std::sync::Arc;
 use std::sync::atomic::AtomicU64;
 use std::sync::atomic::AtomicUsize;
@@ -22,18 +20,16 @@ use bfs::bfs2::Bfs2State;
 use dfs::graph::DfsNode;
 use dfs::lifecycle::DfsLifecycle;
 use dfs::lifecycle::LogLevel;
+use gol::graph::GolEdge;
+use gol::graph::GolGraphParams;
+use gol::graph::GolNode;
+use gol::graph::GolRecenter;
 use gol::lifecycle::GolLifecycle;
 use gol::lifecycle::GolRctlEp;
-use lgol::axis::LGolEdgeRead;
-use lgol::axis::LGolFancyAxis;
-use lgol::bg::LGolBgEmpty;
-use lgol::bg::LGolBgVertStripes;
-use lgol::bg::LGolBgX2;
-use lgol::graph::LGolGraphParams;
 use sal::SerdeFormat;
 
 fn main() {
-    main1::<u16>().unwrap();
+    main1::<u64>().unwrap();
 }
 
 fn main1<B: UScalar + DeserializeOwned + Serialize>() -> Result<(), StringError> {
@@ -42,57 +38,37 @@ fn main1<B: UScalar + DeserializeOwned + Serialize>() -> Result<(), StringError>
     let wx = args.next().unwrap().parse().unwrap();
     let mx = args.next().unwrap().parse().unwrap();
 
-    let ge = LGolGraphParams {
-        vu: (mx, 0, 0),
-        vv: (0, -1, 5),
-        vw: (0, 0, 1),
+    let ge = GolGraphParams {
+        mt: 8,
+        mx: mx,
+        wx: wx,
 
-        bg_coord: PhantomData::<LGolBgX2>,
+        left_edge: GolEdge::Empty,
+        right_edge: GolEdge::Empty,
 
-        u_axis: LGolFancyAxis {
-            w: (wx, mx),
-            left_bg: LGolBgVertStripes(),
-            right_bg: LGolBgEmpty(),
-        },
-        v_axis: (LGolEdgeRead::Wrap, LGolEdgeRead::Wrap),
+        ox: 0,
+        oy: 3,
+
+        recenter: GolRecenter::BiasRight,
     };
-    let mut ge = ge.derived::<[B; 10], _>(HashSet::new());
+    assert!(ge.mt * ge.mx <= B::size());
 
     let st = match args.next() {
         Some(path) => {
             SerdeFormat::Bincode.read(path).unwrap()
         },
         None => {
-            let rs = ge.parse_bs(&[
-                "*...", "*...", "*...", "*...", "*...",
-                "*...", "*...", "*...", "*...", "*...",
-            ]);
-            let (xyt, rs) = ge.recenter_xyt((0, 0, 0), rs);
-            let n0 = ge.regular_node(xyt, rs);
+            let (r0, r1) = ge.parse_and_recenter_pair(
+                "*..*. ..**. ..*.* .**.. *.*.. *.*.. ..**. .***.",
+                "*...* ...** ..... .**.. .*... .*.*. *.... .*.*.",
+            );
+            let n0 = ge.regular_node::<B, ()>(r0, r1);
 
             Bfs2State::new(vec![(vec![n0.key_node().unwrap()], n0)])
         },
     };
 
-    {
-        let ends = vec![
-            // ripped from main greyship
-            &["**....", "*.*...", "*.....", "*.....", "*.....", "*.*...", "*.**..", "*.**..", "*.**..", "*.**.."],
-            &["*.....", "*.....", "*.*...", "*.**..", "*.**..", "*.**..", "*.**..", "*.**..", "*.*...", "*.*.*."],
-            &["*.*...", "*.**..", "*.**..", "*.**..", "*.**..", "*.**..", "*.*.*.", "*.*.*.", "*.*.*.", "*.*.*."],
-
-            // 2c/2 wick edge, interesting if we could find it
-            &["*.**..", "*.*.*.", "*.*...", "*..*..", "*.....", "*.**..", "*.**..", "*...*.", "*.*...", "*.*..."],
-        ];
-        for end in ends {
-            let rs = ge.parse_bs(end);
-            let (xyt, rs) = ge.recenter_xyt((0, 0, 0), rs);
-            let n = ge.key_node(xyt, rs).lgol_hash_node();
-            ge.ends.insert(n);
-        }
-    }
-
-    assert!(ge.max_r1l <= B::size());
+    let ge = ge.derived((), ());
 
     let ep = Arc::new(GolRctlEp {
         threads: AtomicUsize::new(8),
@@ -108,7 +84,7 @@ fn main1<B: UScalar + DeserializeOwned + Serialize>() -> Result<(), StringError>
         ep: ep,
     };
 
-    bfs::bfs2(st, &ge, &mut le);
+    bfs::bfs2::<GolNode<B, _>, _, _>(st, &ge, &mut le);
 
     le.log(LogLevel::INFO, "Done");
 
