87bdd2f2c9ede0c1ed95efb7d46c48cb2432170e
Keith Amling <keith.amling@gmail.com>
2020-08-03 08:05:57 -0700

    LGolBgX2Y2

--- src/lgol/bg.rs
+++ src/lgol/bg.rs
@@ -46,6 +46,14 @@ impl LGolBgCoord for () {
     }
 }
 
+pub trait LGolBgHasX2: LGolBgCoord {
+    fn x2(&self) -> i8;
+}
+
+pub trait LGolBgHasY2: LGolBgCoord {
+    fn y2(&self) -> i8;
+}
+
 #[derive(Clone)]
 #[derive(Copy)]
 #[derive(Debug)]
@@ -85,6 +93,12 @@ impl LGolBgCoord for LGolBgX2 {
     }
 }
 
+impl LGolBgHasX2 for LGolBgX2 {
+    fn x2(&self) -> i8 {
+        self.0
+    }
+}
+
 #[derive(Clone)]
 #[derive(Copy)]
 #[derive(Debug)]
@@ -121,6 +135,84 @@ impl LGolBgCoord for LGolBgY2 {
 
     fn max_idx() -> usize {
         2
+    }
+}
+
+impl LGolBgHasY2 for LGolBgY2 {
+    fn y2(&self) -> i8 {
+        self.0
+    }
+}
+
+#[derive(Clone)]
+#[derive(Copy)]
+#[derive(Debug)]
+#[derive(Default)]
+#[derive(Deserialize)]
+#[derive(Eq)]
+#[derive(Hash)]
+#[derive(Ord)]
+#[derive(PartialEq)]
+#[derive(PartialOrd)]
+#[derive(Serialize)]
+pub struct LGolBgX2Y2(pub i8);
+
+impl LGolBgX2Y2 {
+    fn split(&self) -> (i8, i8) {
+        let x = self.0 % 2;
+        let y = self.0 / 2;
+        (x, y)
+    }
+
+    fn pair(x: i8, y: i8) -> LGolBgX2Y2 {
+        let x = x.rem_euclid(2);
+        let y = y.rem_euclid(2);
+        LGolBgX2Y2(2 * y + x)
+    }
+}
+
+impl LGolBgCoord for LGolBgX2Y2 {
+    fn mul(&self, n: isize) -> LGolBgX2Y2 {
+        let (x, y) = self.split();
+        let x = (n as i8) * x;
+        let y = (n as i8) * y;
+        LGolBgX2Y2::pair(x, y)
+    }
+
+    fn add(&self, other: LGolBgX2Y2) -> LGolBgX2Y2 {
+        let (x1, y1) = self.split();
+        let (x2, y2) = other.split();
+        LGolBgX2Y2::pair(x1 + x2, y1 + y2)
+    }
+
+    fn from_xyt((x, y, _t): Vec3) -> LGolBgX2Y2 {
+        LGolBgX2Y2::pair(x as i8, y as i8)
+    }
+
+    fn to_idx(&self) -> usize {
+        self.0 as usize
+    }
+
+    fn from_idx(idx: usize) -> LGolBgX2Y2 {
+        LGolBgX2Y2(idx as i8)
+    }
+
+    fn max_idx() -> usize {
+        4
+    }
+}
+
+impl LGolBgHasX2 for LGolBgX2Y2 {
+    fn x2(&self) -> i8 {
+        let (x, _y) = self.split();
+        x
+    }
+}
+
+impl LGolBgHasY2 for LGolBgX2Y2 {
+    fn y2(&self) -> i8 {
+        let (_x, y) = self.split();
+        y
     }
 }
 
@@ -170,9 +262,9 @@ impl<BC: LGolBgCoord> LGolBg<BC> for LGolBgEmpty {
 #[derive(Copy)]
 pub struct LGolBgVertStripes();
 
-impl LGolBg<LGolBgX2> for LGolBgVertStripes {
-    fn bg_cell(&self, bg_coord: LGolBgX2) -> bool {
-        bg_coord.0 == 0
+impl<BC: LGolBgHasX2> LGolBg<BC> for LGolBgVertStripes {
+    fn bg_cell(&self, bg_coord: BC) -> bool {
+        bg_coord.x2() == 0
     }
 }
 
@@ -180,8 +272,8 @@ impl LGolBg<LGolBgX2> for LGolBgVertStripes {
 #[derive(Copy)]
 pub struct LGolBgHorizStripes();
 
-impl LGolBg<LGolBgY2> for LGolBgHorizStripes {
-    fn bg_cell(&self, bg_coord: LGolBgY2) -> bool {
-        bg_coord.0 == 0
+impl<BC: LGolBgHasY2> LGolBg<BC> for LGolBgHorizStripes {
+    fn bg_cell(&self, bg_coord: BC) -> bool {
+        bg_coord.y2() == 0
     }
 }
