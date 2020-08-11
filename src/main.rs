672418857d3deeb781ff90fc4b1ed4f53aab4041
Keith Amling <keith.amling@gmail.com>
2020-08-11 14:30:10 -0700

    real search

--- src/main.rs
+++ src/main.rs
@@ -100,17 +100,16 @@ fn main1<B: UScalar + DeserializeOwned + Serialize>(ep: Arc<GolRctlEp>) -> Resul
     let cf = AnonMmapChunkFactory();
     let st = args.read_state_or(Bfs2CustomSerializer(cf), || {
         let rs = ge.parse_bs2(&[
-            // "      |      |      |*.*.**|*.*...",
-            // "*.**..|*.**..|*.*...|*.*...|*.*.*.",
-            // "*.*...|*.*...|*.*...|      |      ",
-            // "z",
-
+            "      |      |      |*.*.**|*.*...",
+            "*.**..|*.**..|*.*...|*.*...|*.*.*.",
+            "*.*...|*.*...|*.*...|      |      ",
+            "z",
 
             // test earlier segment?
-            "     |     |     |*...*|*...*",
-            "**.*.|*.*..|*.**.|*.**.|*.*.*",
-            "*.*..|*.*..|*....|     |     ",
-            "z",
+            // "     |     |     |*...*|*...*",
+            // "**.*.|*.*..|*.**.|*.**.|*.*.*",
+            // "*.*..|*.*..|*....|     |     ",
+            // "z",
         ]);
         let (xyt, rs) = ge.recenter_xyt((0, 0, 0), rs);
         let n0 = ge.regular_node(xyt, rs);
